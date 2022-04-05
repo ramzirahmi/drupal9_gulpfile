2654e03c3ecb4d45e9e1b4f7e8b173ace924d602
ramzi rahmi <ramzirahmi9@gmail.com>
2022-04-05 12:45:40 +0100

    add navbar

--- web/themes/custom/formation_drupal9/assets/js/main.js
+++ web/themes/custom/formation_drupal9/assets/js/main.js
@@ -1,27 +1,27 @@
-(function ($, Drupal) {
-    $("#owl-demo").owlCarousel({
-        loop: true,
-        margin: 60,
-        nav: true,
-        dots: false,
-        responsiveClass: true,
-        stagePadding: 30,
-        autoplay: false,
-        responsive: {
-            0: {
-                items: 2,
-                autoplay: true,
-            },
-            400: {
-                items: 2,
-                autoplay: true,
-            },
-            600: {
-                items: 4,
-            },
-            1000: {
-                items: 6,
-            },
-        },
-    });
-}
+// (function ($, Drupal) {
+//     $("#owl-demo").owlCarousel({
+//         loop: true,
+//         margin: 60,
+//         nav: true,
+//         dots: false,
+//         responsiveClass: true,
+//         stagePadding: 30,
+//         autoplay: false,
+//         responsive: {
+//             0: {
+//                 items: 2,
+//                 autoplay: true,
+//             },
+//             400: {
+//                 items: 2,
+//                 autoplay: true,
+//             },
+//             600: {
+//                 items: 4,
+//             },
+//             1000: {
+//                 items: 6,
+//             },
+//         },
+//     });
+// }
